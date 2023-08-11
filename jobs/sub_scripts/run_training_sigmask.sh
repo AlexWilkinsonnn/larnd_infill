9e5ca6e582b8a59a7f33dd5bb9aef85517ae34b7
Alex Wilkinson <alexander.wilkinson.20@ucl.ac.uk>
2023-08-12 00:23:09 +0100

    slurm script to run experiment

---
+++ jobs/sub_scripts/run_training_sigmask.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -p GPU
+#SBATCH -N1
+#SBATCH -c4
+#SBATCH --gres=gpu:1
+#SBATCH --error=/home/awilkins/larnd_infill/larnd_infill/jobs/logs/err/job%j.err
+#SBATCH --output=/home/awilkins/larnd_infill/larnd_infill/jobs/logs/out/job%j.out
+
+CONFIG_FILE=$1
+
+echo $CONFIG_FILE
+
+cd /home/awilkins/larnd_infill/larnd_infill
+source setups/setup.sh
+
+python ME/train/train_sigmask.py --valid_iter 50 --print_iter 10 $CONFIG_FILE
+
