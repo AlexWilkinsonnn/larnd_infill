59d4b304cf246edccfb2aaed895dec17f9a14e1e
Alex Wilkinson <alexander.wilkinson.20@ucl.ac.uk>
2023-11-03 04:57:21 +0000

    plot script for discriminator loss if available

--- checkpoints/plot_lc.py
+++ checkpoints/plot_lc.py
@@ -1,14 +1,14 @@
 import argparse
 from collections import defaultdict
+from itertools import cycle
 
-from matplotlib import pyplot as plt
-
+from matplotlib import pyplot as plt; from matplotlib.lines import Line2D
 
 def main(args):
     with open(args.loss_file, "r") as f:
         losses = [ loss_line.rstrip() for loss_line in f ]
 
-    training_epochs, training_losses = [], []
+    training_epochs, training_losses, training_losses_D = [], [], defaultdict(list)
     validation_epochs, validation_losses = [], defaultdict(list)
 
     if args.iters_per_epoch == -1:
@@ -18,7 +18,7 @@ def main(args):
         iters_per_epoch = args.iters_per_epoch
 
     for i_line, line in enumerate(losses):
-        if i_line + 3 >= len(losses):
+        if i_line + (3 if args.legacy else 5) >= len(losses):
             break
 
         if line.startswith("Epoch: "):
@@ -28,11 +28,21 @@ def main(args):
             tot_loss = float(loss_line.split("total=")[1].split(" ")[0])
             training_epochs.append(epoch + iter / iters_per_epoch)
             training_losses.append(tot_loss)
+            if "D_tot" in loss_line:
+                training_losses_D["D_tot"].append(
+                    float(loss_line.split("D_tot=")[1].split(" ")[0])
+                )
+                training_losses_D["D_real"].append(
+                    float(loss_line.split("D_real=")[1].split(" ")[0])
+                )
+                training_losses_D["D_fake"].append(
+                    float(loss_line.split("D_fake=")[1].split(" ")[0])
+                )
 
         if line == "== Validation Loop ==":
             loss_line = losses[i_line + 2]
             tot_loss = float(loss_line.split("total=")[1].split(" ")[0])
-            epoch_line = losses[i_line + (5 if not args.legacy else 3)]
+            epoch_line = losses[i_line + (3 if args.legacy else 5)]
             epoch = int(epoch_line.split("Epoch ")[1].split(" ")[0])
             validation_epochs.append(epoch)
             validation_losses["total"].append(tot_loss)
@@ -44,15 +54,48 @@ def main(args):
                     loss_name = loss_line_comps.pop().split("=")[0]
                     validation_losses[loss_name].append(weighted_loss)
 
-    plt.plot(training_epochs, training_losses, label="train - total")
+    fig, ax = plt.subplots()
+
+    prop_cycle = plt.rcParams['axes.prop_cycle']
+    colors = cycle(prop_cycle.by_key()['color'])
+
+    ax.plot(training_epochs, training_losses, label="train - total", c=next(colors))
     validation_losses_total = validation_losses.pop("total")
-    plt.plot(validation_epochs, validation_losses_total, label="valid - total")
+    ax.plot(validation_epochs, validation_losses_total, label="valid - total", c=next(colors))
     for loss_name, losses in validation_losses.items():
-        plt.plot(validation_epochs, losses, "--", label="valid - {} (weighted)".format(loss_name))
-    plt.xlabel("Epoch")
-    plt.ylabel("loss")
-    plt.ylim(0, 1.2 * max(validation_losses_total))
-    plt.legend()
+        ax.plot(
+            validation_epochs, losses, "--",
+            label="valid - {} (weighted)".format(loss_name), c=next(colors)
+        )
+    ax.set_xlabel("Epoch")
+    ax.set_ylabel("loss")
+    ax.set_ylim(0, 1.2 * max(validation_losses_total))
+
+    if training_losses_D:
+        ax2 = ax.twinx()
+        for loss_name, losses in training_losses_D.items():
+            ax2.plot(training_epochs, losses, "-.", label=loss_name, c=next(colors))
+        ax2.set_ylabel("D loss")
+        ax2.set_ylim(0, 1.2 * max(max(training_losses_D.values(), key=lambda losses: max(losses))))
+
+        ax_ylims = ax.axes.get_ylim()
+        ax_yratio = ax_ylims[0] / ax_ylims[1]
+        ax2_ylims = ax2.axes.get_ylim()
+        ax2_yratio = ax2_ylims[0] / ax2_ylims[1]
+        if ax_yratio < ax2_yratio:
+            ax2.set_ylim(bottom=ax2_ylims[1] * ax_yratio)
+        else:
+            ax.set_ylim(bottom=ax_ylims[1] * ax2_yratio)
+
+        handles, labels = ax.get_legend_handles_labels()
+        handles2, labels2 = ax2.get_legend_handles_labels()
+        handles += handles2
+        labels += labels2
+        new_handles = [Line2D([], [], c=h.get_color()) for h in handles]
+        plt.legend(handles=new_handles, labels=labels, ncol=2)
+    else:
+        plt.legend(ncol=2)
+
     plt.show()
 
 
