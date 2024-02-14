3c03d9f29e0dc3e1083a0a2433aca17fad8e2b02
Alex Wilkinson <alexander.wilkinson.20@ucl.ac.uk>
2024-02-14 16:01:59 +0000

    update make pointclouds slurm script

--- jobs/sub_scripts/run_make_pointclouds.sh
+++ jobs/sub_scripts/run_make_pointclouds.sh
@@ -2,8 +2,9 @@
 #SBATCH -p RCIF
 #SBATCH -N1
 #SBATCH -c4
+#SBATCH -J make_larnd_voxelmaps
+#SBATCH --array 1-10
 #SBATCH --mem=10000
-#SBATCH --nodelist=compute-0-18
 #SBATCH --error=/home/awilkins/larnd_infill/larnd_infill/jobs/logs/err/job%j.err
 #SBATCH --output=/home/awilkins/larnd_infill/larnd_infill/jobs/logs/out/job%j.out
 
@@ -11,19 +12,20 @@ INPUT_DIR=$1
 OUTPUT_DIR=$2
 VMAP_PATH=$3
 
-echo $INPUT_DIR
-echo $OUTPUT_DIR
-echo $VMAP_PATH
+echo "Job id ${SLURM_JOB_ID}"
+echo "Job array task id ${SLURM_ARRAY_TASK_ID}"
+echo "Running on ${SLURM_JOB_NODELIST}"
+echo "input dir is ${INPUT_DIR}"
+echo "output dir is ${OUTPUT_DIR}"
+echo "voxel map is ${VMAP_PATH}"
 
-# Make sure they have trailing forward slashes
-# INPUT_DIR=$(echo $INPUT_DIR | sed 's#[^/]$#&/#')
-# OUTPUT_DIR=$(echo $OUTPUT_DIR | sed 's#[^/]$#&/#')
+input_name=$(ls $INPUT_DIR | head -n $SLURM_ARRAY_TASK_ID | tail -n -1)
+input_file=${INPUT_DIR}/${input_name}
+
+echo "input file is ${input_file}"
 
 cd /home/awilkins/larnd_infill/larnd_infill
 source setups/setup.sh
 
-for file in ${INPUT_DIR%/}/*; do
-  echo $file
-  python data_scripts/make_larnd_pointclouds.py --batch_mode $file $OUTPUT_DIR $VMAP_PATH
-done
+python data_scripts/make_larnd_pointclouds.py --batch_mode $input_file $OUTPUT_DIR $VMAP_PATH
 
