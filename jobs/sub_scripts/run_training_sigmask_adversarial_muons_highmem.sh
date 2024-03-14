1e6193571db1c91852eec067e9a589b48799b0ce
Alex Wilkinson <alexander.wilkinson.20@ucl.ac.uk>
2024-03-14 17:52:31 +0000

    use constraint to select high memory gpus only

--- jobs/sub_scripts/run_training_sigmask_adversarial_muons_highmem.sh
+++ jobs/sub_scripts/run_training_sigmask_adversarial_muons_highmem.sh
@@ -2,7 +2,8 @@
 #SBATCH -p GPU
 #SBATCH -N1
 #SBATCH -c4
-#SBATCH --gres=gpu:l40s:1
+#SBATCH --gres=gpu:1
+#SBATCH --constraint="a100|l40s"
 #SBATCH --error=/home/awilkins/larnd_infill/larnd_infill/jobs/logs/err/job%j.err
 #SBATCH --output=/home/awilkins/larnd_infill/larnd_infill/jobs/logs/out/job%j.out
 
