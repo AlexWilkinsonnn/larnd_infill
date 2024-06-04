f384cf7dfbe1d98deba65ef604d8321cedac9673
Alex Wilkinson <alexander.wilkinson.20@ucl.ac.uk>
2024-06-04 23:25:50 +0100

    update plotting script

--- checkpoints/plot_3d_points.py
+++ checkpoints/plot_3d_points.py
@@ -1,5 +1,4 @@
 import argparse
-from itertools import cycle
 
 import matplotlib; from matplotlib import pyplot as plt
 import yaml
@@ -19,21 +18,54 @@ def main(args):
     fig = plt.figure()
     ax = fig.add_subplot(projection="3d")
 
-    norm_feats = matplotlib.colors.Normalize(
-        vmin=min(feat[0] for feat in target_points.values()),
-        vmax=max(feat[0] for feat in target_points.values())
-    )
-    m_feats = matplotlib.cm.ScalarMappable(norm=norm_feats, cmap=matplotlib.cm.viridis)
+    # norm_feats = matplotlib.colors.Normalize(
+    #     vmin=-max(feat[0] for feat in target_points.values()),
+    #     vmax=max(feat[0] for feat in target_points.values())
+    # )
+    # m_feats = matplotlib.cm.ScalarMappable(norm=norm_feats, cmap=matplotlib.cm.coolwarm)
 
     x_size = vmap["x_step_target"]
     y_size = vmap["y_step_target"]
     z_size = vmap["z_step_target"]
-    x_size_draw = x_size / 5
-    y_size_draw = x_size / 5
-    z_size_draw = z_size / 2
+    x_size_draw = x_size
+    y_size_draw = y_size
+    z_size_draw = z_size
+    gap_coords = set()
+    for coords, feat in in_points.items():
+        adc = int(float(feat[0]) * (1 / args.in_scalefactor))
+
+        x_bin = vmap["x"][coords[0]]
+        x_pos = (x_bin[1] + x_bin[0]) / 2
+        y_bin = vmap["y"][coords[1]]
+        y_pos = (y_bin[1] + y_bin[0]) / 2
+        z_bin = vmap["z"][coords[2]]
+        z_pos = (z_bin[1] + z_bin[0]) / 2
+
+        if int(feat[-1]):
+            gap_coords.add((x_bin, y_bin, z_bin))
+
+        if adc < 6:
+            continue
+
+        if (
+            not (args.xlow < x_pos < args.xhigh) or
+            not (args.ylow < y_pos < args.yhigh) or
+            not (args.zlow < z_pos < args.zhigh)
+        ):
+            continue
+
+        x, y, z = get_cube()
+        x = x * x_size_draw + x_pos
+        y = y * y_size_draw + y_pos
+        z = z * z_size_draw + z_pos
+
+        # c = m_feats.to_rgba(adc)
+
+        ax.plot_surface(x, z, y, color="Blue", shade=False)
+
     for coords, feat in pred_points.items():
-        feat = int(feat[0])
-        if feat < 16:
+        adc = int(feat[0])
+        if adc < 6:
             continue
 
         x_bin = vmap["x"][coords[0]]
@@ -43,14 +75,24 @@ def main(args):
         z_bin = vmap["z"][coords[2]]
         z_pos = (z_bin[1] + z_bin[0]) / 2
 
+        if (x_bin, y_bin, z_bin) not in gap_coords:
+            continue
+
+        if (
+            not (args.xlow < x_pos < args.xhigh) or
+            not (args.ylow < y_pos < args.yhigh) or
+            not (args.zlow < z_pos < args.zhigh)
+        ):
+            continue
+
         x, y, z = get_cube()
-        x = x * x_size_draw + (x_pos * x_size)
-        y = y * y_size_draw + (y_pos * y_size)
-        z = z * z_size_draw + (z_pos * z_size)
+        x = x * x_size_draw + x_pos
+        y = y * y_size_draw + y_pos
+        z = z * z_size_draw + z_pos
 
-        c = m_feats.to_rgba(feat)
+        # c = m_feats.to_rgba(-adc)
 
-        ax.plot_surface(x, z, y, color=c, shade=False)
+        ax.plot_surface(x, z, y, color="Green", shade=False)
 
     ax.set_box_aspect((4,8,4))
     ax.grid(False)
@@ -60,11 +102,10 @@ def main(args):
     ax.w_yaxis.set_pane_color((1.0, 1.0, 1.0, 1.0))
     ax.w_zaxis.set_pane_color((1.0, 1.0, 1.0, 1.0))
     ax.set_xlabel("x")
-    ax.set_ylabel("y")
-    ax.set_zlabel("z")
+    ax.set_ylabel("z")
+    ax.set_zlabel("y")
 
     plt.show()
-
 
 def get_cube():
     """Get coords for plotting cuboid surface with Axes3D.plot_surface"""
@@ -77,7 +118,6 @@ def get_cube():
 
     return x,y,z
 
-
 def parse_arguments():
     parser = argparse.ArgumentParser()
 
@@ -86,8 +126,15 @@ def parse_arguments():
     parser.add_argument("in_points_yml", type=str)
     parser.add_argument("voxel_map", type=str)
 
+    parser.add_argument("--xlow", type=float, default=-np.inf)
+    parser.add_argument("--xhigh", type=float, default=np.inf)
+    parser.add_argument("--ylow", type=float, default=-np.inf)
+    parser.add_argument("--yhigh", type=float, default=np.inf)
+    parser.add_argument("--zlow", type=float, default=-np.inf)
+    parser.add_argument("--zhigh", type=float, default=np.inf)
+    parser.add_argument("--in_scalefactor", type=float, default=1.0)
+
     return parser.parse_args()
 
 if __name__ == '__main__':
     main(parse_arguments())
-
